95bcc8d2944b525fe28995cbff519b8471327fce
mmuuripe <markus.muuripeal@gmail.com>
2024-04-11 17:13:50 +0300

    refactor of controller

--- Controllers/RequestController.cs
+++ Controllers/RequestController.cs
@@ -1,18 +1,21 @@
 using Microsoft.AspNetCore.Mvc;
 using RequestManager.Models;
+using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Lib.AspNetCore.ServerSentEvents;
 
-
-namespace RequestManager.Controllers {
-    public class RequestController : Controller {
+namespace RequestManager.Controllers 
+{
+    public class RequestController : Controller 
+    {
         private readonly RequestService _requestService;
         private readonly SSEService _sseService;
 
         public RequestController(RequestService requestService, SSEService sseService)
         {
-        _requestService = requestService;
-        _sseService = sseService;
+            _requestService = requestService;
+            _sseService = sseService;
         }
 
         [HttpGet]
@@ -25,40 +28,58 @@ namespace RequestManager.Controllers {
             }
             catch (Exception ex)
             {
-                var requestId = Guid.NewGuid().ToString();
-
-                var errorViewModel = new ErrorViewModel { RequestId = requestId };
-
-                return View("Error", errorViewModel);
+                return HandleError(ex);
             }
         }
 
         [HttpPost]
-        public async Task<IActionResult> CreateRequest(RequestModel request) {
-            try {
-                if (ModelState.IsValid) {
+        public async Task<IActionResult> CreateRequest(RequestModel request) 
+        {
+            try 
+            {
+                if (ModelState.IsValid) 
+                {
                     _requestService.CreateRequest(request);
-                    await _sseService.SendEventAsync(new ServerSentEvent { Type = "requests_updated", Data = new List<string>{""}  });
+                    await _sseService.SendEventAsync(new ServerSentEvent { Type = "requests_updated", Data = new List<string>{""} });
                     return Ok();
-                } else {
+                } 
+                else 
+                {
                     return BadRequest(ModelState);
                 }
-            } catch (Exception ex) {
-                Console.WriteLine($"An error occurred while creating the request: {ex.Message}");
-                return StatusCode(500, "An error occurred while processing your request.");
+            } 
+            catch (Exception ex) 
+            {
+                return HandleError(ex);
             }
         }
 
         [HttpDelete]
-        public async Task<IActionResult> DeleteRequest(int id) {
-            try {
+        public async Task<IActionResult> DeleteRequest(int id) 
+        {
+            try 
+            {
                 _requestService.DeleteRequest(id);
                 await _sseService.SendEventAsync(new ServerSentEvent { Type = "requests_updated", Data = new List<string>{""} });
                 return Ok();
-            } catch (Exception ex) {
-                Console.WriteLine($"An error occurred while deleting the request: {ex.Message}");
-                return StatusCode(500, "An error occurred while processing your request.");
+            } 
+            catch (Exception ex) 
+            {
+                return HandleError(ex);
             }
+        }
+
+        private IActionResult HandleError(Exception ex) 
+{
+            Console.WriteLine($"An error occurred: {ex.Message}");
+
+            var errorViewModel = new ErrorViewModel
+            {
+                RequestId = Guid.NewGuid().ToString(),
+                // You can add more properties to the ErrorViewModel if needed
+            };
+
+            return View("Error", errorViewModel);
         }
     }
 }
