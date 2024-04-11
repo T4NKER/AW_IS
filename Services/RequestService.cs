620ef58a5180288375f933d4b24be33830c17b30
mmuuripe <markus.muuripeal@gmail.com>
2024-04-11 17:14:18 +0300

    improved coherency and methods in RequestService

--- Services/RequestService.cs
+++ Services/RequestService.cs
@@ -1,55 +1,71 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using RequestManager.Models;
 
 public class RequestService
 {
-    private List<RequestModel> _supportRequests;
+    private List<RequestModel> _supportRequests = new List<RequestModel>();
 
     public RequestService()
     {
-        _supportRequests = new List<RequestModel>();
-        for (int i = 1; i <= 12; i++) {
-                _supportRequests.Add(new RequestModel(i, $"Sample Request {i}", DateTime.Now,
-                                                     DateTime.Now.AddDays(1)));
-        }
+        CreateSupportRequests();
+    }
+
+    private void CreateSupportRequests()
+    {
+        _supportRequests = Enumerable.Range(1, 12)
+                                     .Select(i => new RequestModel(i, $"Sample Request {i}", DateTime.Now, DateTime.Now.AddDays(1)))
+                                     .ToList();
     }
 
     public void CreateRequest(RequestModel request)
     {
-        if (request.Description == "") {
-            throw new ArgumentException("Description cannot be empty");
-        }
-        if (request.DeadlineTime <= DateTime.Now.AddHours(1)) {
-            throw new ArgumentException("Deadline time cannot be before current time");
-        }
-        request.Id = _supportRequests.Count > 0 ? _supportRequests.Max(r => r.Id) + 1 : 1;
-                    request.RequestTime = DateTime.Now;
+        ValidateRequest(request);
 
-                    _supportRequests.Add(request);
+        request.Id = _supportRequests.Count > 0 ? _supportRequests.Max(r => r.Id) + 1 : 1;
+        request.RequestTime = DateTime.Now;
+
+        _supportRequests.Add(request);
     }
 
     public void DeleteRequest(int id)
     {
         var request = _supportRequests.FirstOrDefault(sr => sr.Id == id);
-                if (request != null) {
-                    _supportRequests.Remove(request);
-                }
+        if (request != null)
+        {
+            _supportRequests.Remove(request);
+        }
     }
 
     public IEnumerable<RequestModel> GetRequests(int id)
     {
-        if (id <= 0) 
+        if (id <= 0)
         {
-            id = 1; 
+            id = 1;
         }
         int pageSize = 10;
-                var activeRequests = _supportRequests.OrderBy(sr => sr.DeadlineTime)
-                                         .Skip((id - 1) * pageSize)
-                                         .Take(pageSize);
+        var activeRequests = _supportRequests.OrderBy(sr => sr.DeadlineTime)
+                                             .Skip((id - 1) * pageSize)
+                                             .Take(pageSize);
         return activeRequests;
     }
 
     public IEnumerable<RequestModel> GetAllRequests()
     {
-    return _supportRequests;
+        return _supportRequests;
+    }
+
+    private void ValidateRequest(RequestModel request)
+    {
+        if (string.IsNullOrWhiteSpace(request.Description))
+        {
+            throw new ArgumentException("Description cannot be empty");
+        }
+
+        if (request.DeadlineTime <= DateTime.Now.AddHours(1))
+        {
+            throw new ArgumentException("Deadline time cannot be before current time or within one hour");
+        }
     }
 }
